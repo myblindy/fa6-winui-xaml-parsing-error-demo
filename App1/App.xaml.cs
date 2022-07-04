7b668d34885c411224e25716048092d6163b8413
myblindy <tweety.04.01@gmail.com>
2022-07-04 16:33:56 -0400

    Add project files.

---
+++ App1/App.xaml.cs
@@ -0,0 +1,75 @@
+﻿using App1.Activation;
+using App1.Contracts.Services;
+using App1.Core.Contracts.Services;
+using App1.Core.Services;
+using App1.Helpers;
+using App1.Services;
+using App1.ViewModels;
+using App1.Views;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.UI.Xaml;
+
+// To learn more about WinUI3, see: https://docs.microsoft.com/windows/apps/winui/winui3/.
+namespace App1;
+
+public partial class App : Application
+{
+    // The .NET Generic Host provides dependency injection, configuration, logging, and other services.
+    // https://docs.microsoft.com/dotnet/core/extensions/generic-host
+    // https://docs.microsoft.com/dotnet/core/extensions/dependency-injection
+    // https://docs.microsoft.com/dotnet/core/extensions/configuration
+    // https://docs.microsoft.com/dotnet/core/extensions/logging
+    private static readonly IHost _host = Host
+        .CreateDefaultBuilder()
+        .ConfigureServices((context, services) =>
+        {
+            // Default Activation Handler
+            services.AddTransient<ActivationHandler<LaunchActivatedEventArgs>, DefaultActivationHandler>();
+
+            // Other Activation Handlers
+
+            // Services
+            services.AddSingleton<IActivationService, ActivationService>();
+            services.AddSingleton<IPageService, PageService>();
+            services.AddSingleton<INavigationService, NavigationService>();
+
+            // Core Services
+            services.AddSingleton<IFileService, FileService>();
+
+            // Views and ViewModels
+            services.AddTransient<MainViewModel>();
+            services.AddTransient<MainPage>();
+
+            // Configuration
+        })
+        .Build();
+
+    public static T GetService<T>()
+        where T : class
+    {
+        return _host.Services.GetService(typeof(T)) as T;
+    }
+
+    public static Window MainWindow { get; set; } = new Window() { Title = "AppDisplayName".GetLocalized() };
+
+    public App()
+    {
+        InitializeComponent();
+        UnhandledException += App_UnhandledException;
+    }
+
+    private void App_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        // TODO: Log and handle exceptions as appropriate.
+        // For more details, see https://docs.microsoft.com/windows/winui/api/microsoft.ui.xaml.unhandledexceptioneventargs.
+    }
+
+    protected async override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        base.OnLaunched(args);
+        var activationService = App.GetService<IActivationService>();
+        await activationService.ActivateAsync(args);
+    }
+}
---
+++ App1/Contracts/Services/IActivationService.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading.Tasks;
+
+namespace App1.Contracts.Services;
+
+public interface IActivationService
+{
+    Task ActivateAsync(object activationArgs);
+}
